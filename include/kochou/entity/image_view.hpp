8c8acd63413c3ab1c8077a92966b01477223f5da
urlagushka <nikolai.turkin.main@gmail.com>
2026-05-20 22:20:00 +0300

    image_view

--- include/kochou/entity/image_view.hpp
+++ include/kochou/entity/image_view.hpp
@@ -1,14 +1,18 @@
 #ifndef KOCHOU_ENTITY_IMAGE_VIEW_HPP
 #define KOCHOU_ENTITY_IMAGE_VIEW_HPP
 
+#include <span>
+
 #include <ktl/memory.hpp>
 
+#include <kochou/entity/image.hpp>
 #include <kochou/entity/interface.hpp>
 
 namespace kochou::entity
 {
 class image_view
 {
+    // requirements
 public:
     static ktl::errc
     ensure(kochou::shared_context _sctx) noexcept;
@@ -16,7 +20,34 @@ public:
     should(kochou::shared_context _sctx) noexcept;
     static bool
     allowed(kochou::shared_context _sctx) noexcept;
+
+    // fabrics
+public:
+    ktl::result< std::vector< kochou::entity::image_view >, ktl::errc >
+    make(kochou::shared_context _sctx, std::span< kochou::entity::image > _images, ktl::api::format _format,
+         ktl::api::image_aspect_flag_bits _aspect = ktl::api::image_aspect_flag_bits::v_color_bit) noexcept;
+
+    // common
+public:
+    image_view(kochou::shared_context _sctx, ktl::api::image_view _image_view, bool _is_need_destroy) noexcept;
+    image_view(const image_view &) noexcept = delete;
+    image_view(image_view &&) noexcept      = default;
+    image_view &
+    operator=(const image_view &) noexcept = delete;
+    image_view &
+    operator=(image_view &&) noexcept = default;
+    ~image_view() noexcept;
+
+    // raw
+public:
+    ktl::api::image_view raw;
+
+    // shared raii
+private:
+    kochou::shared_context sctx_;
+    bool                   is_need_destroy_;
 };
+
 using shared_image_view = ktl::memory::sptr< image_view >;
 } // namespace kochou::entity
 
---
+++ src/entity/image_view.cpp
@@ -0,0 +1,80 @@
+#include <kochou/context/view.hpp>
+#include <kochou/entity/image_view.hpp>
+
+ktl::errc
+kochou::entity::image_view::ensure(kochou::shared_context _sctx) noexcept
+{
+    return ktl::errc::success;
+}
+
+ktl::errc
+kochou::entity::image_view::should(kochou::shared_context _sctx) noexcept
+{
+    return ktl::errc::success;
+}
+
+bool
+kochou::entity::image_view::allowed(kochou::shared_context _sctx) noexcept
+{
+    return true;
+}
+
+ktl::result< std::vector< kochou::entity::image_view >, ktl::errc >
+kochou::entity::image_view::make(
+    kochou::shared_context _sctx, std::span< kochou::entity::image > _images, ktl::api::format _format,
+    ktl::api::image_aspect_flag_bits _aspect = ktl::api::image_aspect_flag_bits::v_color_bit) noexcept
+{
+    auto device = kochou::view::device(_sctx);
+
+    std::vector< image_view > image_views(_images.size());
+    for (ktl::usize i = 0; i < _images.size(); ++i)
+    {
+        ktl::api::image_view_create_info create_info{};
+        create_info.image                              = _images[i].raw;
+        create_info.view_type                          = ktl::api::image_view_type::v_2d;
+        create_info.format                             = _format;
+        create_info.components.r                       = ktl::api::component_swizzle::v_identity;
+        create_info.components.g                       = ktl::api::component_swizzle::v_identity;
+        create_info.components.b                       = ktl::api::component_swizzle::v_identity;
+        create_info.components.a                       = ktl::api::component_swizzle::v_identity;
+        create_info.subresource_range.aspect_mask      = static_cast< ktl::api::image_aspect_flags >(_aspect);
+        create_info.subresource_range.base_mip_level   = 0;
+        create_info.subresource_range.level_count      = 1;
+        create_info.subresource_range.base_array_layer = 0;
+        create_info.subresource_range.layer_count      = 1;
+
+        ktl::api::image_view view_raw = nullptr;
+        ktl::api::result     rc       = ktl::api::create_image_view(device, &create_info, nullptr, &view_raw);
+        if (rc != ktl::api::result::v_success)
+        {
+            for (auto & view : image_views)
+            {
+                if (view.raw)
+                {
+                    ktl::api::destroy_image_view(device, view.raw, nullptr);
+                }
+            }
+            return ktl::err(ktl::cast_api_result(rc));
+        }
+
+        image_views[i] = image_view(_sctx, view_raw, true);
+    }
+
+    return image_views;
+}
+
+kochou::entity::image_view::image_view(kochou::shared_context _sctx, ktl::api::image_view _image_view,
+                                       bool _is_need_destroy) noexcept
+    : raw(_image_view), sctx_(_sctx), is_need_destroy_(_is_need_destroy)
+{
+}
+
+kochou::entity::image_view::~image_view() noexcept
+{
+    if (raw && is_need_destroy_)
+    {
+        auto device = kochou::view::device(sctx_);
+        ktl::api::destroy_image_view(device, raw, nullptr);
+        raw = ktl::api::image_view{};
+    }
+}
